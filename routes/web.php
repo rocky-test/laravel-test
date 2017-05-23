4ccf5bfa4a55aefb43dd5ea0d7b56bc522a7da78
Rokas Balbokas <ciholir@envy17.com>
2017-05-23 20:18:50 +0300

    added routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'FrontController@index')->name('home');
+Route::get('/shirts', 'FrontController@shirts')->name('shirts');
+Route::get('/shirt/{id}', 'FrontController@shirt')->name('shirt');
